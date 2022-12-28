867097b5f10b380f33d143e34fe36f3bbd4fa854
Ismael <>
2022-12-28 00:51:39 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
-// const clip = document.querySelectorAll(".projects .all-projects video");
+
 const header = document.querySelector(".header.container");
+//TRYING AUTOPLAY
+// const clip = document.querySelectorAll(".projects .all-projects video");
+// trying to fix video gitch
+//const vid = document.querySelectorAll(".projects .all-projects video");
+
 
 
 hamburger.addEventListener("click",()=>{
@@ -23,6 +28,12 @@ menu_item.forEach((item)=>{
     mobile_menu.classList.toggle("active"); 
     });
 });
+// trying to fix video gitch
+// vid.addEventListener("click", (e)=>{
+//     let player = e.target;
+//     player.width = player.clientWidth;
+//     player.height = player.clientHeight;
+// })
 
 // for(let i =0; i<clip.length; i++){
 //     clip[i].addEventListener("mouseenter", function(e){
